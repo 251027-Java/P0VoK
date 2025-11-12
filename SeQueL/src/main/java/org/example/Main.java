128dd129442f4cd435e672a9d86f288cdb9f9fb1
Kenneth Vo <kennethvo2003@gmail.com>
2025-11-12 16:49:13 -0600

    brainstormed structure. may change

--- SeQueL/src/main/java/org/example/Main.java
+++ SeQueL/src/main/java/org/example/Main.java
@@ -1,17 +1,42 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+
         }
     }
 }
+
+/* tables?
+movies
+- id prim key
+- tmdb id?
+- title
+- overview
+- release date
+- rating 5 stars
+- year created
+users?
+- id
+- username
+- review count
+m:m
+- movie id
+- user id
+prim(movie, userid)
+ */
+
+/* classes? sep files
+user.java
+- get functions
+- thats it
+movie.java
+watchlist? act as m to m???
+repo layer - userrepo
+- use resultset connect to api? prob look up how api works first
+- use postgres
+movierrepo? maybe to keep both sets of data separated
+service
+- one for movie and service if we do two for repos
+ */

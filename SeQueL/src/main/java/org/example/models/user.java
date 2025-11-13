8b41f1989f4d59271d938810809c1dba95bb9761
Kenneth Vo <kennethvo2003@gmail.com>
2025-11-13 11:58:51 -0600

    commit before lunch lol

--- SeQueL/src/main/java/org/example/models/user.java
+++ SeQueL/src/main/java/org/example/models/user.java
@@ -20,13 +20,17 @@ public class user {
     }
 
     // methods
-    public int getUserID() {
-        return userID;
-    }
-    public void setUserID(int userID) {
-        this.userID = userID;
-    }
-    public String getUsername() {
-        return username;
+    public int getUserID() { return userID; }
+    public void setUserID(int userID) { this.userID = userID; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public Date getDate() { return date; }
+    public void setDate(Date date) { this.date = date; }
+
+    @Override
+    public String toString() {
+        return "User: " + "userID : " + userID +
+                "\nusername : " + username +
+                "\ndate : " + date;
     }
 }

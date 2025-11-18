0d5980e788875525c7ccab190feed90d73cfdf23
Kenneth Vo <kennethvo2003@gmail.com>
2025-11-17 20:44:20 -0600

    finished userRepo and fixed/added better organization to user class

--- SeQueL/src/main/java/org/example/models/user.java
+++ SeQueL/src/main/java/org/example/models/user.java
@@ -1,31 +1,46 @@
 package org.example.models;
 
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
 public class user {
 
     // fields
-    private int userID;
+    private Integer userID;
     private String username;
-    private Date date;
+    private LocalDateTime date;
 
     // default con
     public user() {}
 
     // overload con
-    public user(int userID, String username, Date date) {
+    public user(Integer userID, String username, LocalDateTime date) {
         this.userID = userID;
         this.username = username;
         this.date = date;
     }
 
     // methods
-    public int getUserID() { return userID; }
-    public void setUserID(int userID) { this.userID = userID; }
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
-    public Date getDate() { return date; }
-    public void setDate(Date date) { this.date = date; }
+    public Integer getUserID() {
+        return userID;
+    }
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
 
     @Override
     public String toString() {
--- SeQueL/src/main/java/org/example/repos/userRepo.java
+++ SeQueL/src/main/java/org/example/repos/userRepo.java
@@ -1,16 +1,105 @@
 package org.example.repos;
 
 import java.sql.*;
+import java.util.Optional;
+
+import org.example.models.user;
 
 public class userRepo {
-    private String url;
-    private String username;
-    private String pass;
-    private Connection connection;
+    private DatabaseConnection dbConn;
 
     public userRepo() {
-
+        this.dbConn = DatabaseConnection.getInstance();
     }
 
+    public user create(user u) throws SQLException {
+        String sql = "INSERT INTO users (username, date) VALUES (?, ?) RETURNING userID";
+        Connection c = null;
+        try {
+            c = dbConn.getConn();
+            PreparedStatement stmt = c.prepareStatement(sql);
+
+            stmt.setString(1, u.getUsername());
+            stmt.setTimestamp(2, Timestamp.valueOf(u.getDate()));
+
+            ResultSet r = stmt.executeQuery();
+            if (r.next()) u.setUserID(r.getInt("userID"));
+
+            return u;
+        } finally {
+            dbConn.releaseConn(c);
+        }
+    }
+
+    public Optional<user> readID(Integer userID) throws SQLException {
+        String sql = "SELECT * FROM users WHERE userID = ?";
+        Connection c = null;
+        try {
+            c = dbConn.getConn();
+            PreparedStatement stmt = c.prepareStatement(sql);
+
+            stmt.setInt(1, userID);
+
+            ResultSet r = stmt.executeQuery();
+            if (r.next()) Optional.of(mapRS(r));
+
+            return Optional.empty();
+        } finally {
+            dbConn.releaseConn(c);
+        }
+    }
+
+    public Optional<user> readName(String username) throws SQLException {
+        String sql = "SELECT * FROM users WHERE username = ?";
+        Connection c = null;
+        try {
+            c = dbConn.getConn();
+            PreparedStatement stmt = c.prepareStatement(sql);
+
+            stmt.setString(1, username);
+
+            ResultSet r = stmt.executeQuery();
+            if (r.next()) Optional.of(mapRS(r));
+
+            return Optional.empty();
+        } finally {
+            dbConn.releaseConn(c);
+        }
+    }
+
+    public void update(user u) throws SQLException {
+        String sql = "UPDATE users SET username = ? WHERE userID = ?";
+        Connection c = null;
+        try {
+            c = dbConn.getConn();
+            PreparedStatement stmt = c.prepareStatement(sql);
+
+            stmt.setString(1, u.getUsername());
+            stmt.setInt(2, u.getUserID());
+
+            stmt.executeUpdate();
+        } finally {
+            dbConn.releaseConn(c);
+        }
+    }
+
+    public void delete(Integer userID) throws SQLException {
+        String sql = "DELETE FROM users WHERE userID = ?";
+        Connection c = null;
+        try {
+            c = dbConn.getConn();
+            PreparedStatement stmt = c.prepareStatement(sql);
+
+            stmt.setInt(1, userID);
+
+            stmt.executeUpdate();
+        } finally {
+            dbConn.releaseConn(c);
+        }
+    }
+
+    private user mapRS(ResultSet r) throws SQLException {
+        return new user(r.getInt("userID"), r.getString("username"), r.getTimestamp("date").toLocalDateTime());
+    }
 
 }

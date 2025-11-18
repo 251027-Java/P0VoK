ac6dfff9c0c27d107ef12b7abe85cf17fa43bacf
Kenneth Vo <kennethvo2003@gmail.com>
2025-11-17 18:29:22 -0600

    dinner break

--- SeQueL/src/main/java/org/example/repos/DatabaseConnection.java
+++ SeQueL/src/main/java/org/example/repos/DatabaseConnection.java
@@ -1,4 +1,24 @@
 package org.example.repos;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private String url = "temp";
+    private String username = "temp";
+    private String pass = "temp";
+
+    private List<Connection> connPool;
+
+    private DatabaseConnection() {
+        connPool = new ArrayList<>(5);
+
+        try {
+            for (int i = 0; i < 5; i++) connPool.add(DriverManager.getConnection(url, username, pass));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }

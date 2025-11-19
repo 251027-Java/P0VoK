87ec2d96a107b348ab165f12f357ba3c5395153c
Kenneth Vo <kennethvo2003@gmail.com>
2025-11-19 15:17:25 -0600

    i have to make a genre class and repo

--- SeQueL/src/main/java/org/example/service/movieService.java
+++ SeQueL/src/main/java/org/example/service/movieService.java
@@ -1,4 +1,31 @@
 package org.example.service;
 
+import org.example.models.movie;
+import org.example.repos.movieRepo;
+import java.sql.*;
+import java.util.*;
+
 public class movieService {
+    private movieRepo movieRepo;
+
+    public movieService() {
+        this.movieRepo = new movieRepo();
+    }
+
+    public Optional<movie> getMovieID(int movieID) {
+        try {
+            Optional<movie> opt = movieRepo.readID(movieID);
+
+            if (opt.isEmpty()) {
+                return Optional.empty();
+            }
+
+            movie m = opt.get();
+            return Optional.of(m);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return Optional.empty();
+    }
 }
